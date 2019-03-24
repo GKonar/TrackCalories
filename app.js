7c2fc908555d32616e7083182ca272f1abc3888e
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2019-03-24 18:10:47 +0100

    Add funcionality for adding element to the list aand to the UI

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // ***** Storage Controller *****
+// ******************************
 
-// ***** Item Controller ***** 
-// Function which is building single food / meal item
+
+// ***** Item Controller *****
+// *************************** 
+// Function which is creating single food / meal item
 const ItemCtrl = (function() {
     //Item Constructor
     const Item = function(id, name, calories) {
@@ -13,9 +16,9 @@ const ItemCtrl = (function() {
     // Data Structure / State
     const data = {
         items: [
-            {id: 0, name: 'Steak Dinner', calories: 1200},
-            {id: 1, name: 'Cookie', calories: 400},
-            {id: 2, name: 'Eggs', calories: 300},
+            // {id: 0, name: 'Steak Dinner', calories: 1200},
+            // {id: 1, name: 'Cookie', calories: 400},
+            // {id: 2, name: 'Eggs', calories: 300},
         ],
         currentItem: null,
         totalCalories: 0
@@ -23,6 +26,30 @@ const ItemCtrl = (function() {
 
     // Public methods
     return {
+        getItems: function () { 
+            return data.items;
+        },
+        addItem: function (name, calories) {
+            let ID;
+            // Create ID
+            if(data.items.length > 0) {
+                ID = data.items[data.items.length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Calories to number
+            calories = parseInt(calories);
+
+            // Create new item 
+            newItem = new Item(ID, name, calories)
+
+            // Add to items array
+            data.items.push(newItem);
+
+            return newItem;
+
+        },
         logData: function() {
             return data;
         }
@@ -32,11 +59,72 @@ const ItemCtrl = (function() {
 
 
 // ***** UI Controller *****
+// *************************
 const UICtrl = (function() {
+    const UISelectors = {
+        itemList: '#item-list',
+        addBtn: '.add-btn',
+        itemNameInput: '#item-name',
+        itemCaloriesInput: '#item-calories'
+    }
     
     //Public methods
     return {
+        populateItemList: function(items) { // This gonna be useful when i add LS
+            let html = '';
 
+            // Looping through items and adding single li to html variable
+            items.forEach((item)=> {
+                html += 
+                `
+                <li class="collection-item" id="item-${item.id}">
+                    <strong>${item.name} </strong> <em>${item.calories} Calories</em>
+                    <a href="#" class="secondary-content">
+                        <i class="edit-item fa fa-pencil"></i>
+                    </a>
+                </li>
+                `;
+            });
+
+            // Insert list items in UI
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+        getItemInput: function() {
+            return {
+                name: document.querySelector(UISelectors.itemNameInput).value,
+                calories: document.querySelector(UISelectors.itemCaloriesInput).value
+            }
+        },
+        addListItem: function(item) {
+            // Show the list
+            document.querySelector(UISelectors.itemList).style.display = 'block';
+            // Create li element
+            const li = document.createElement('li');
+            // Add class
+            li.className = 'collection-item';
+            // Add ID
+            li.id = `item-${item.id}`;
+            // Add HTML
+            li.innerHTML = 
+            `
+                <strong>${item.name} </strong> <em>${item.calories} Calories</em>
+                <a href="#" class="secondary-content">
+                    <i class="edit-item fa fa-pencil"></i>
+                </a>
+            `
+            // Insert item
+            document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li);
+        },
+        clearInput: function () {
+            document.querySelector(UISelectors.itemNameInput).value = '';
+            document.querySelector(UISelectors.itemCaloriesInput).value = '';
+        },
+        hideList: function () {
+            document.querySelector(UISelectors.itemList).style.display = 'none';
+        },
+        getSelectors: function() {
+            return UISelectors;
+        }
     }
 
 })();
@@ -44,12 +132,54 @@ const UICtrl = (function() {
 
 
 // ***** App Controller *****
+// **************************
 const App = (function(ItemCtrl, UICtrl) {
+    // Load event listeners
+    const loadEventListeners = function () {
+
+        // Get UI selectors from UI controller
+        const UISelectors = UICtrl.getSelectors()
+
+        // Add item event 
+        document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+    };
+
+    // Add item submit (from btn event listener)
+    const itemAddSubmit = function(e) {
+        // get form input from UI Controller
+        const input = UICtrl.getItemInput();
+
+        // Check for name and calories input
+        if (input.name !== '' && input.calories !== '') {
+            // Add item 
+            const newItem = ItemCtrl.addItem(input.name, input.calories);
+            // Add item to UI list
+            UICtrl.addListItem(newItem);
+
+            // Clear fields
+            UICtrl.clearInput();
+        }
+
+        e.preventDefault()
+    };
+
 
     // Public methods
     return {
         init: function() {
-            console.log('Initializing app...')
+            // Fetch items from data structure
+            const items = ItemCtrl.getItems();
+            
+            // Check if any items 
+            if(items.length === 0) {
+                UICtrl.hideList();
+            } else {
+                // Populate list with items
+                UICtrl.populateItemList(items);
+            }
+            
+            // Load event listeners
+            loadEventListeners();
         }
     }
     
